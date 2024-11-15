a0fc9dc476aec5527bd0e1f10424b4c4bd51225a
Omkar Harade <omkarharade1301@gmail.com>
2024-11-15 23:19:35 +0530

    home to assessment page redirect added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,18 @@
+"use client";
+
+import { useEffect } from "react";
+import { redirect } from "next/navigation";
+
 export default function Home() {
+	useEffect(() => {
+		redirect("/assessments");
+	}, []);
+
 	return (
-		<div>
-			<h1>The home page</h1>
+		<div className="flex w-[100%] h-[10rem] bg-orange-300 justify-center items-center">
+			<h1 className="font-extrabold text-xl">
+				Please wait you are being directed . . . . .
+			</h1>
 		</div>
 	);
 }
